0086467edc810c0aa0707ec0cddf71699e266796
DESKTOP-RAQTNP4\Admin <admin>
2026-02-17 21:40:28 +0800

    no message

--- src/entities/IGroupRequirementGovernmentIds.ts
+++
@@ -1,7 +0,0 @@
-import { IRequirementGovernmentIds } from "./IRequirementGovernmentIds";
-
-export interface IGroupRequirementGovernmentIds extends IDefault {
-  label: string;
-  minRequirement: number;
-  require: IRequirementGovernmentIds[];
-}
